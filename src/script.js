693c4c8e485dfd4ad7d7e1802cdff5e0c43b54a1
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2022-05-11 15:05:28 -0300

    Atualizações sobre a página de doações

--- src/script.js
+++ src/script.js
@@ -1,14 +1,10 @@
 
-
-
-
 // Função para aplicar reuso da navbar e do final
 $(document).ready(function () {
-    $("#navbar").load("menu.html", function () {
+    $("#header").load("reuso.html", function () {
         $("#btn").on('click', showmenu);
     });
 });
-
-function showmenu() {
+function shownavbar() {
     navigator.classList.toggle('active');
 }
