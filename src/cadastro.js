d5a0fc3adb031bae3e7adf37a1aab75783703eef
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2022-05-17 15:32:02 -0300

    Update

--- src/cadastro.js
+++ src/cadastro.js
@@ -27,58 +27,58 @@ if (isset($_POST['cadastrar'])) {
     $verifyn = mysqli_query($connect, "SELECT * FROM cadastro WHERE usuario = '{$Usuário}'");
 
     if (strlen($Nome) < 2) {
-        echo "<h2>
+        echo`<h2>
         Nome inválido!
             <
-            /h2>";
-    }
+            /h2>`;
+    };
     elseif(strlen($Usuário) < 2) {
-        echo "<h2>
+        echo`<h2>
         Usuário inválido!
             <
-            /h2>";
-    }
+            /h2>`;
+    };
     elseif(strlen($Email) < 10) {
-        echo "<h2>
+        echo`<h2>
         Email inválido!
             <
-            /h2>";
-    }
+            /h2>`;
+    };
     elseif(strlen($senha) < 8) {
-        echo "<h2>
+        echo`<h2>
         Senha inválida!
             <
-            /h2>";
-    }
+            /h2>`;
+    };
     elseif($csenha != $senha) {
-        echo "<h2>
+        echo`<h2>
         Senhas distintas!
             <
-            /h2>";
-    }
+            /h2>`;
+    };
     elseif(mysqli_num_rows($verify) > 0) {
-        echo "<h2>
+        echo`<h2>
         Email já registrado!
             <
-            /h2>";
-    }
+            /h2>`;
+    };
     elseif(mysqli_num_rows($verifyn) > 0) {
-        echo "<h2>
+        echo`<h2>
         Usuário já existente!
             <
-            /h2>";
-    }
+            /h2>`;
+    };
     else {
         $insers = mysqli_query($connect, "INSERT INTO cadastro(nome, usuario, email, senha) VALUES('$Nome', '$Usuário', '$Email', MD5'$senha')");
         if ($insers) {
-            echo "<h2>
+            echo`<h2>
             Cadastrado com êxito, bem vindo!
                 <
-                /h2>"; 
+                /h2>`;
         } else {
-            echo "Erro";
-        }
-    }
+            echo`Erro`;
+        };
+    };
 
 
 }
