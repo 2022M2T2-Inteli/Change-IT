69739edf921c407de230683325a86ba8271c9b3d
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2022-05-23 15:09:44 -0300

    update banco de dados e reusos

--- backend/app_22.js
+++ backend/app_22.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express();
-const hostname = '127.0.0.1';
-const port = 3022;
-var db = require("./connection.js");
+const express = require('express'); // framework express 
+const app = express(); // app faz o manuseio do express
+const hostname = '127.0.0.1'; // endereço 
+const port = 3022; // porta do site
+var db = require("./connection.js"); // db recebe tudo que tá no javascript do banco de dados
 
-app.use(express.static("../src/"));
+app.use(express.static("../src/")); // pega o diretório do front
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/Home.html`);
+  console.log(`Server running at http://${hostname}:${port}/Home.html`); // printa no console
 });
--- backend/connection.js
+++ backend/connection.js
@@ -1,21 +1,21 @@
-var sqlite3 = require('sqlite3').verbose()
-var md5 = require('md5')
+var sqlite3 = require('sqlite3').verbose() // recebe o módulo do sqlite
+var md5 = require('md5') // recebe o módulo do md5 (criptografia)
 
-const DBSOURCE = "Projeto5.db"
+const DBSOURCE = "Projeto5.db" // responsável pela operação do bd
 
 let db = new sqlite3.Database(DBSOURCE, (err) => {
-    if (err) {
+    if (err) { // aparece o erro no console se ele existir
         // Cannot open database
         console.error(err.message)
         throw err
     } else {
-        console.log('Connected to the SQLite database.')
+        console.log('Connected to the SQLite database.') // aparece isso no console se der bom
         // var insert = 'INSERT INTO user (name, email, password) VALUES (?,?,?)'
         // db.run(insert, ["admin", "admin@example.com", md5("admin123456")])
         // db.run(insert, ["user", "user@example.com", md5("user123456")])
-
+        // db.run(insert, ["zaidan", "zaidan@alexandre.kil", md5("ste123456")])
     }
 });
 
 
-module.exports = db
+module.exports = db // exporta o bd
