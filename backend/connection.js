cb503f6e236e3772823be5aa58c99edc71ae931a
Alexandre Fonseca <80794067+Xandebrabe@users.noreply.github.com>
2022-05-23 02:08:43 -0300

    Correções backend

--- backend/connection.js
+++ backend/connection.js
@@ -1,7 +1,7 @@
 var sqlite3 = require('sqlite3').verbose()
 var md5 = require('md5')
 
-const DBSOURCE = "projeto5.db"
+const DBSOURCE = "Projeto5.db"
 
 let db = new sqlite3.Database(DBSOURCE, (err) => {
     if (err) {
@@ -10,23 +10,10 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
         throw err
     } else {
         console.log('Connected to the SQLite database.')
-        db.run(`CREATE TABLE user (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name text, 
-            email text UNIQUE, 
-            password text, 
-            CONSTRAINT email_unique UNIQUE (email)
-            )`,
-            (err) => {
-                if (err) {
-                    // Table already created
-                } else {
-                    // Table just created, creating some rows
-                    var insert = 'INSERT INTO user (name, email, password) VALUES (?,?,?)'
-                    db.run(insert, ["admin", "admin@example.com", md5("admin123456")])
-                    db.run(insert, ["user", "user@example.com", md5("user123456")])
-                }
-            });
+        var insert = 'INSERT INTO user (name, email, password) VALUES (?,?,?)'
+        db.run(insert, ["admin", "admin@example.com", md5("admin123456")])
+        db.run(insert, ["user", "user@example.com", md5("user123456")])
+
     }
 });
 
