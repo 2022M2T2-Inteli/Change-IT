487ff36ae4fd0f6bd48e6a93a0f184981b224451
Alexandre Fonseca <80794067+Xandebrabe@users.noreply.github.com>
2022-05-24 14:33:12 -0300

    adding modules to the api

--- backend/app_22.js
+++ backend/app_22.js
@@ -2,6 +2,7 @@ const express = require('express'); // framework express
 const app = express(); // app faz o manuseio do express
 const hostname = '127.0.0.1'; // endereço 
 const port = 3022; // porta do site
+
 var db = require("./connection.js"); // db recebe tudo que tá no javascript do banco de dados
 
 app.use(express.static("../src/")); // pega o diretório do front
